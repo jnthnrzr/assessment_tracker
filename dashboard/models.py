b9678dd072a3c1a012697235253c53e4c8efa6c1
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-27 22:00:25 -0400

    Add models for Assessment, Question, Choice

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Assessment(models.Model):
+    title = models.CharField(max_length=140)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=140)
+    assessment = models.ForeignKey(Assessment,
+                                   related_name='questions',
+                                   on_delete=models.CASCADE,
+                                   null=False)
+
+
+class Choice(models.Model):
+    text = models.CharField(max_length=40)
+    question = models.ForeignKey(Question,
+                                 related_name='choices',
+                                 on_delete=models.CASCADE,
+                                 null=False)
+    correct = models.BooleanField(default=False)
