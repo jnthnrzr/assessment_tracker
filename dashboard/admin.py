377eeec2b7a3b76556d44a43fee7ead0f7c287c9
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-28 07:58:20 -0400

    List and add choices for each Question in admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Assessment, Choice, Question
 
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ['assessment', 'text']}),
+    )
+    inlines = (ChoiceInline, )
+
+
 admin.site.register(Assessment)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
