35562df7a96c5a754e5d93891a798521553ab05e
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-28 19:21:49 -0400

    Create UserAssessment model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from dashboard.models import Assessment
 
-# Create your models here.
+
+class UserAssessment(models.Model):
+    user = models.ForeignKey(User, related_name='assessments',
+                             on_delete=models.CASCADE, null=False,)
+    assessment = models.ForeignKey(Assessment, related_name="users",
+                                   on_delete=models.CASCADE, null=False,)
+    score = models.IntegerField(default=0, null=True,)
