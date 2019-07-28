6e9cb2dc4e45fb42f1d1ccd6030330df2c0edea5
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-28 19:16:44 -0400

    Connect accounts urls to project urls

--- assessment_tracker/urls.py
+++ assessment_tracker/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('dashboard/', include('dashboard.urls')),
+    path('', include('accounts.urls')),
+    path('', include('dashboard.urls')),
 ]
