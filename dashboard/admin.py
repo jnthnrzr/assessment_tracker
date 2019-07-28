b70de1187d4b70a44bbe2e7d78179b9dd828d139
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-27 22:08:08 -0400

    Register Assessment, Question, Choice to admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Assessment, Choice, Question
 
-# Register your models here.
+admin.site.register(Assessment)
+admin.site.register(Question)
+admin.site.register(Choice)
