710f7e32201dc61f6bf1ccc1809966c302cba2b0
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-27 22:08:58 -0400

    Create ViewSets for Assessment, Question, Choice

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Assessment, Choice, Question
+from .serializers import (
+    AssessmentSerializer, ChoiceSerializer, QuestionSerializer
+)
+
+
+class AssessmentViewSet(viewsets.ModelViewSet):
+    queryset = Assessment.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = AssessmentSerializer
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = QuestionSerializer
+
+
+class ChoiceViewSet(viewsets.ModelViewSet):
+    queryset = Choice.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = ChoiceSerializer
