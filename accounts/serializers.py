4293ea85685527e4010a9f62102ad2ea157b321a
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-29 12:44:13 -0400

    Keep username as required field for accounts serializers for now

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,28 +7,30 @@ class UserSerializer(serializers.ModelSerializer):
     """User Serializer"""
     class Meta:
         model = User
-        fields = ('id', 'email')
+        fields = ('id', 'username', 'email')
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     """Register Serializer"""
     class Meta:
         model = User
-        fields = ('id', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            validated_data['email'],
-            validated_data['password']
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
         )
         return user
 
 
 class LoginSerializer(serializers.Serializer):
     """Login Serializer"""
+    username = serializers.CharField()
     email = serializers.CharField()
-    password = serializers.CharField()
+    password = serializers.CharField(style={'input_type': 'password'})
 
     def validate(self, data):
         user = authenticate(**data)
