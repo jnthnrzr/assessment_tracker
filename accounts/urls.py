bf6a2e7d29a51a4c24daa97d320a28c1685811bc
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-29 15:32:42 -0400

    Include userassessment urls in accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from knox import views as knox_views
+from rest_framework import routers
+from .views import RegisterAPI, LoginAPI, UserAPI, UserAssessmentViewSet
 
-from .views import RegisterAPI, LoginAPI, UserAPI
+assessment_router = routers.DefaultRouter()
+assessment_router.register(prefix='api/userassessments',
+                viewset=UserAssessmentViewSet,
+                basename='userassessments')
 
 urlpatterns = [
   path('api/auth', include('knox.urls')),
@@ -9,4 +14,5 @@ urlpatterns = [
   path('api/auth/login', LoginAPI.as_view()),
   path('api/auth/user', UserAPI.as_view()),
   path('api/auth/logout', knox_views.LogoutView.as_view(), name="knox_logout"),
+  path('', include(assessment_router.urls)),
 ]
