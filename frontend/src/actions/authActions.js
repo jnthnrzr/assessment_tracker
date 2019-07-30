4221477fc3307a1dce5e5d1dee0fd6d2fbf53fca
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-30 18:59:13 -0400

    Write actions for loginUser, registerUser, logoutUser

---
+++ frontend/src/actions/authActions.js
@@ -0,0 +1,94 @@
+import axios from "axios";
+
+import {
+  CLEAR_ASSESSMENTS,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT_SUCCESS,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+} from './types';
+
+// LOGIN USER
+export const loginUser = (email, password) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  // Request Body
+  const body = JSON.stringify({ email, password });
+
+  axios
+  .post("/api/auth/login", body, config)
+  .then(res => {
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+  })
+  .catch(err => {
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  });
+};
+
+// REGISTER USER
+export const registerUser = ({ email, password }) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  // Request Body
+  const body = JSON.stringify({ email, password });
+
+  axios
+  .post("/api/auth/register", body, config)
+  .then(res => {
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
+  })
+  .catch(err => {
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  });
+};
+
+// LOGOUT USER
+export const logoutUser = () => (dispatch, getState) => {
+  axios
+  .post("/api/auth/logout/", null, tokenConfig(getState))
+  .then(() => {
+    dispatch({ type: CLEAR_ASSESSMENTS });
+    dispatch({ type: LOGOUT_SUCCESS });
+  })
+  .catch(err => console.log(err));
+};
+
+// Setup config with token - helper function
+export const tokenConfig = getState => {
+  // Get token from state
+  const token = getState().authReducer.token;
+
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  // If token, add to headers config
+  if (token) {
+    config.headers["Authorization"] = `Token ${token}`;
+  }
+  return config;
+};
