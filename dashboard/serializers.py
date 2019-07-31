7e9d90c0b6d6053986cb41e5d2e1a49383fad4c4
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-07-30 23:21:25 -0400

    Use nested serializer for Assessment model

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -3,22 +3,26 @@ from rest_framework import serializers
 from .models import Assessment, Choice, Question, UserAssessment
 
 
-class AssessmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Assessment
-        fields = '__all__'
-
-
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = '__all__'
-
-
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = '__all__'
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ['text', 'choices']
+
+
+class AssessmentSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Assessment
+        fields = ['title', 'questions']
 
 
 class UserAssessmentSerializer(serializers.ModelSerializer):
