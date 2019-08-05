6584ccf929992884331f01fe089de1f89e1749e3
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-08-04 21:51:27 -0400

    Include individual assessment page in App

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import store from '../store';
@@ -7,22 +7,32 @@ import Dashboard from './Dashboard';
 import Landing from './Landing';
 import Login from './Login';
 import Register from './Register';
+import Assessment from "./Assessment";
+import { loadUser } from '../actions/authActions';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Route exact path="/" component={Landing} />
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <PrivateRoute exact path="/assessments" component={Dashboard} />
-          </Switch>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <Fragment>
+            <Route exact path="/" component={Landing}/>
+            <Switch>
+              <PrivateRoute exact path="/assessments" component={Dashboard}/>
+              <PrivateRoute exact path="/assessment/:id"
+                            component={Assessment}/>
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/register" component={Register}/>
+            </Switch>
+          </Fragment>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
