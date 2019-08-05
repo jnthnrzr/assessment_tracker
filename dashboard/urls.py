fe404e2a945e8942590c92097af58c38ffa98c8b
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-08-04 21:48:46 -0400

    Connect url for best score among assessments

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 
-from .views import (AssessmentViewSet, ChoiceViewSet, QuestionViewSet,
-                    UserAssessmentViewSet)
+from .views import (AssessmentViewSet, BestScoreViewSet, ChoiceViewSet,
+                    QuestionViewSet, UserAssessmentViewSet)
 
 router = routers.DefaultRouter()
 router.register(prefix='api/assessments', viewset=AssessmentViewSet,
@@ -12,4 +12,7 @@ router.register(prefix='api/choices', viewset=ChoiceViewSet,
                 basename='choices',)
 router.register(prefix='api/userassessments', viewset=UserAssessmentViewSet,
                 basename='userassessments')
+router.register(prefix='api/best', viewset=BestScoreViewSet,
+                basename='best')
+
 urlpatterns = router.urls
