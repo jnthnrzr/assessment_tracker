2b0f14b895a81997203699b16dfd122ae98a303d
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-08-04 21:47:07 -0400

    Show summary when assessment is completed.

--- frontend/src/components/Assessment.js
+++ frontend/src/components/Assessment.js
@@ -1,43 +1,103 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
+import {
+  checkAnswer,
+  finishAssessment,
+  incrementQuestion,
+  loadQuestions,
+  updateAssessment,
+} from "../actions/assessmentsActions";
+
+function Summary(props) {
+  return (
+    <div>
+      <h3>Assessments</h3>
+      <div>
+        <h4>You scored {props.score}.</h4>
+        <p>{props.correctCount}/{props.total} correct</p>
+      </div>
+      <button type="button" onClick={props.handleClick}>
+        Done
+      </button>
+    </div>
+  );
+}
 
 class Assessment extends Component {
   state = {
-    option: "",
+    option: null,
   };
 
   handleChange = event => {
     this.setState({
-      option: event.target.value.toLowerCase(),
+      option: parseInt(event.target.value, 10),
     });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    console.log("You have submitted:", this.state.option);
+    const { option } = this.state;
+    if (!option) return;
+
+    const { questions, questionIdx, currentAssessment } = this.props;
+    const { choices } = questions[questionIdx];
+    const guess = choices.find(choice => choice.id === option);
+
+    if (guess.correct) {
+      const { updateAssessment, currentScore, user } = this.props;
+      const assessment = {
+        'id': currentAssessment.id,
+        "user": user.id,
+        "assessment": currentAssessment.assessment,
+        "score": currentScore + 10,
+      };
+      updateAssessment(assessment);
+    }
+
+    const { incrementQuestion } = this.props;
+    incrementQuestion(questionIdx);
   };
 
+  componentDidMount() {
+    const { loadQuestions, currentAssessment } = this.props;
+    loadQuestions(currentAssessment.assessment);
+  }
+
   render() {
-    const { assessment, questionIdx } = this.props;
-    const { title, questions } = assessment;
+    const { currentAssessment, questionIdx, finishAssessment } = this.props;
+
+    if (!currentAssessment) {
+      return <Redirect to="/assessments"/>;
+    }
+
+    const { title } = currentAssessment;
+    const { questions } = this.props;
     const question = questions[questionIdx];
+
+    if (!question) {
+      return <Summary handleClick={finishAssessment}
+                      correctCount={currentAssessment.score/10}
+                      total={questions.length}
+                      score={currentAssessment.score} />;
+    }
+
     const { choices } = question;
 
     return (
       <div>
         <h2>{title}</h2>
-        <h4>{this.state.option}</h4>
 
         <h3>{question.text}</h3>
         <form onSubmit={this.handleSubmit}>
-          {choices.map(choice => (
+          {choices.map((choice) => (
             <div className="form-check" key={choice.id}>
               <label>
                 <input
-                  type="radio"
+                  type="checkbox"
                   name="choice"
-                  value={choice.text}
-                  checked={this.state.option === choice.text.toLowerCase()}
+                  value={choice.id}
+                  checked={this.state.option === choice.id}
                   onChange={this.handleChange}
                   className="form-check-input"
                 />
@@ -60,8 +120,18 @@ class Assessment extends Component {
 const mapStateToProps = state => ({
   user: state.authReducer.user,
   isAuthenticated: state.authReducer.isAuthenticated,
-  assessment: state.assessmentsReducer.currentAssessment,
+  currentAssessment: state.assessmentsReducer.currentAssessment,
+  currentScore: state.assessmentsReducer.currentScore,
+  questions: state.assessmentsReducer.questions,
   questionIdx: state.assessmentsReducer.currentQuestionIdx,
 });
 
-export default connect(mapStateToProps)(Assessment);
+const mapDispatchToProps = {
+  checkAnswer,
+  finishAssessment,
+  loadQuestions,
+  incrementQuestion,
+  updateAssessment,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Assessment);
